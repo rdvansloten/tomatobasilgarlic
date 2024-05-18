d5d4e67d3bb7d6df16537797c5e8a996851bbe74
Rudy van Sloten <rdvansloten@outlook.com>
2024-05-18 14:58:51 +0200

    Remove unused variables.

--- api/FlaskApp/__init__.py
+++ api/FlaskApp/__init__.py
@@ -3,21 +3,18 @@ from flask import Flask, redirect, request, session, jsonify, Response
 import logging
 from datetime import datetime, timezone, timedelta, time as dtime
 import sys
-from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 
 # Environment variables
 managed_identity_client_id = os.environ["MANAGED_IDENTITY_CLIENT_ID"]
-key_vault_name = os.environ["KEY_VAULT_NAME"]
-home_url = f"yourdomain." + os.environ["HOME_URL"]
 
 # Authentication
 managed_identity_credential = DefaultAzureCredential(managed_identity_client_id=managed_identity_client_id, exclude_interactive_browser_credential=False, additionally_allowed_tenants="*")
 
 # Retrieve Key Vault Secrets
-key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
-key_vault_client = SecretClient(vault_url=key_vault_uri, credential=managed_identity_credential)
+# key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
+# key_vault_client = SecretClient(vault_url=key_vault_uri, credential=managed_identity_credential)
 
 # Logging Settings
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename='function.log', encoding='utf-8', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
@@ -25,14 +22,14 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 # Start app
 app = Flask(__name__)
-app.config["SECRET_KEY"] = key_vault_client.get_secret("flask-secret-key").value
-app.config['SESSION_COOKIE_DOMAIN'] = f".{home_url}"
+app.config["SECRET_KEY"] = "TEST" # key_vault_client.get_secret("flask-secret-key").value
+app.config['SESSION_COOKIE_DOMAIN'] = "tomatobasilgarlic.azurewebsites.net"
 app.config["SESSION_COOKIE_NAME"] = "your-session"
 app.config["REMEMBER_COOKIE_DOMAIN"] = "None"
 
 @app.route("/")
 def home():
-    return "Not allowed."
+    return "Hello, world."
 
 @app.route("/status")
 def status():
@@ -40,14 +37,14 @@ def status():
         session["access_token"]
         logging.info("Access token found")
         result = {
-        "rc": "loggedin",
-        "msg": f"User is logged in with access token {session['access_token']}."
+            "rc": "loggedin",
+            "msg": f"User is logged in with access token {session['access_token']}."
         }
     except:
         logging.info("No access token found")
         result = {
-        "rc": "notloggedin",
-        "msg": f"User is not logged in."
+            "rc": "notloggedin",
+            "msg": f"User is not logged in."
         }
         
     return jsonify(result)
